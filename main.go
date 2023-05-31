676dd7496056008c8d938905a57c3102af70bcf7
Dimas Ardianto <dimasardnt6@gmail.com>
2023-05-31 14:43:23 +0700

    push

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 // @contact.email fiber@swagger.io
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
+// @host  https://dimasardnt6-ulbi.herokuapp.com
 // @BasePath /
 
 func main() {
