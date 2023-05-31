55947d76f89d8663b7ac722a487b4cdf9e5b9dca
Dimas Ardianto <dimasardnt6@gmail.com>
2023-05-31 14:45:35 +0700

    push

--- docs/docs.go
+++ docs/docs.go
@@ -140,7 +140,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "https://dimasardnt6-ulbi.herokuapp.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Fiber Example API",
