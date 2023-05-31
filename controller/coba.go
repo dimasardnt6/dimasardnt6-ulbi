654e78dde21b5416d88638733dba53929a1e220b
Dimas Ardianto <dimasardnt6@gmail.com>
2023-05-31 14:50:54 +0700

    push

--- controller/coba.go
+++ controller/coba.go
@@ -156,6 +156,19 @@ func GetAllPresensi(c *fiber.Ctx) error {
 	ps := inimodullatihan.GetAllPresensi(config.Ulbimongoconn2, "presensi")
 	return c.JSON(ps)
 }
+
+// GetPresensiID godoc
+// @Summary Get By ID Data Presensi.
+// @Description Ambil per ID data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 404
+// @Failure 500
+// @Router /presensi/{id} [get]
 func GetPresensiID(c *fiber.Ctx) error {
 	id := c.Params("id")
 	if id == "" {
