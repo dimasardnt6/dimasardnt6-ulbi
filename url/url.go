0f851ebc980ac0454a5a459cb6cea324240799a6
Dimas Ardianto <dimasardnt6@gmail.com>
2023-06-20 17:03:48 +0700

    push

--- url/url.go
+++ url/url.go
@@ -45,10 +45,12 @@ func Web(page *fiber.App) {
 
 	// Endpoint Antrian Puskesmas
 	// Get Endpoint
+	page.Get("/all-user", controller.GetAllUser)
 	page.Get("/all-pasien", controller.GetAllPasien)
 	page.Get("/all-antrian", controller.GetAllAntrian)
 	page.Get("/all-poliklinik", controller.GetAllPoliklinik)
 	page.Get("/all-dokter", controller.GetAllDokter)
+	page.Get("/pasien/:id", controller.GetUserFromID)           //menampilkan data pasien berdasarkan id
 	page.Get("/pasien/:id", controller.GetPasienFromID)         //menampilkan data pasien berdasarkan id
 	page.Get("/antrian/:id", controller.GetAntrianFromID)       //menampilkan data antrian berdasarkan id
 	page.Get("/poliklinik/:id", controller.GetPoliklinikFromID) //menampilkan data poliklinik berdasarkan id
